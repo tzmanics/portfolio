2996f4f6ef7c356c52f22a672cdb00e80e15af23
tara manicsic <tzmanics@gmail.com>
2014-02-02 00:55:33 -0500

    +mucho better slider placeholder tbc

--- script.js
+++ script.js
@@ -3,4 +3,18 @@ $(document).ready(function(){
 	sectionContainer: "section",
 	responsiveFallback: 600
 	});
+	$('#camera_wrap_1').camera({
+		thumbnails: true
+	});
+
+	$('#camera_wrap_2').camera({
+		thumbnails: true
+	});
+
+	$('#camera_wrap_3').camera({
+		thumbnails: true
+	});
+	$('#camera_wrap_4').camera({
+		thumbnails: true
+	});
 });
